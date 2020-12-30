f8bfdd05110f7f4092167b37182af8438cf2a51a
Austin-Vincelli-Evans <unity7591@gmail.com>
2020-12-30 16:56:24 -0700

    context, sidebar, modal, complete

--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,15 @@ import React, { useContext } from "react";
 import { FaBars } from "react-icons/fa";
 import { AppContext, useGlobalContext } from "./context";
 const Home = () => {
-  const data = useGlobalContext();
+  const { openSidebar, openModal } = useGlobalContext();
   return (
     <main>
-      <button className="sidebar-toggle">
-        {data}
+      <button className="sidebar-toggle" onClick={openSidebar}>
         <FaBars />
       </button>
-      <button className="btn">Show Modal</button>
+      <button className="btn" onClick={openModal}>
+        Show Modal
+      </button>
     </main>
   );
 };
